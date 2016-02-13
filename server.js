7deeaeb0ab95f3676498d12d2b1bbcf8fe90c7a8
z639 <z639@htmlchat.net>
2016-02-13 15:24:52 +0000

    update 112

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	  response.write("Hi \n\n");
-	  response.end("The server is active \n");
+	  response.end("The server is active. \n");
 });
 
 server.listen( port, ipaddress, function() {
